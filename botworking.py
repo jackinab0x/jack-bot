e854e621a180778922cc184cb955ecb61d624c5e
jackinab0x <31683388+jackinab0x@users.noreply.github.com>
2018-01-17 17:38:19 +0530

    Update botworking.py

--- botworking.py
+++ botworking.py
@@ -23,6 +23,8 @@ async def my_background_task():
             if(not temp==main):
                 await client.send_message(channel, ':alien:'+ '`New post from reddit-`'+'\n'+'**'+unidecode.unidecode(key1["title"]+'**'+'\n'+unidecode.unidecode(key1["link"])))
                 temp=main
+            else:
+                print("No new feed-")
         await asyncio.sleep(30) # task runs every 30 seconds
 
 @client.event
@@ -33,5 +35,12 @@ async def on_ready():
     print('------')
     await client.change_presence(game=discord.Game(name='Reddit feed'))
 @client.event
+async def on_message(message):
+    if message.content.startswith('!pubg'):
+        msg=message.content[6:]
+        for x in range(0,len(msg)):
+            if msg[x]==' ':
+                name=msg[:x]
+                region=msg[x+1:len(msg)]
 client.loop.create_task(my_background_task())
 client.run('')
